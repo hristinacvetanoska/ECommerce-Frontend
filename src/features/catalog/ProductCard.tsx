7502a0657e4ee50de5db38975f5337f4d3e902fa
hristinacvetanoska <72557639+hristinacvetanoska@users.noreply.github.com>
2022-09-18 20:43:42 +0200

    added switch for dark mode

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -16,17 +16,28 @@ export default function ProductCard({ product }: Props) {
   return (
     <Card>
       <CardHeader
-        avatar={<Avatar>{product.name.charAt(0).toUpperCase()}</Avatar>}
+        avatar={
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
         title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: "bold", color: "primary.main" },
+        }}
       />
       <CardMedia
-        sx={{ height: 140 }}
+        sx={{
+          height: 140,
+          backgroundSize: "contain",
+          ///bgcolor: "primary.light",
+        }}
         image={product.pictureUrl}
         title={product.name}
       />
       <CardContent>
         <Typography gutterBottom color={"secondary"} variant="h5">
-          {product.price}
+          {product.price.toFixed(2)} мкд
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand}/{product.type}
