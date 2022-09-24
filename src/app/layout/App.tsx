07eaa1479fdfecdc0b07d3f53dfbbcc237210557
hristinacvetanoska <72557639+hristinacvetanoska@users.noreply.github.com>
2022-09-25 00:18:15 +0200

    error handling

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -5,14 +5,17 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
-
+import "react-toastify/dist/ReactToastify.css";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? "dark" : "light";
@@ -29,14 +32,19 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" theme="colored" hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/catalog" component={Catalog} />
-        <Route exact path="/catalog/:id" component={ProductDetails} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/contact" component={ContactPage} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/catalog" component={Catalog} />
+          <Route exact path="/catalog/:id" component={ProductDetails} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route exact path="/server-error" component={ServerError} />
+          <Route exact component={NotFound} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );
