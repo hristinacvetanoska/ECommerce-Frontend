c98df6fce3e227adb6e06bdbae8d54b7dd4f8c21
hristinacvetanoska <72557639+hristinacvetanoska@users.noreply.github.com>
2022-11-06 16:17:38 +0100

    Redux functionalities

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,20 @@ import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserHistory } from "history";
 import { Router } from "react-router-dom";
-import { StoreProvider } from "./app/context/StoreContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store/configureStore";
+
 export const history = createBrowserHistory();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Router history={history}>
-      <StoreProvider>
+      <Provider store={store}>
         <App />
-      </StoreProvider>
+      </Provider>
     </Router>
   </React.StrictMode>
 );
