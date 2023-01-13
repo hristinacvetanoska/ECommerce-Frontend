d77e3fa4e4222b35fd3f0ae1e4522d04c0d9988a
hristinacvetanoska <72557639+hristinacvetanoska@users.noreply.github.com>
2023-01-13 01:00:34 +0100

    deleted cоmmented lines оf cоde

--- src/features/checkout/PaymentForm.tsx
+++ src/features/checkout/PaymentForm.tsx
@@ -17,51 +17,6 @@ export default function PaymentForm() {
         <Grid item xs={12} md={6}>
           Cash
         </Grid>
-
-        {/*  <Grid item xs={12} md={6}>
-          <AppTextInput
-            name="nameOnCard"
-            label="Name on card"
-            control={control}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            // required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            //required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            //required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>*/}
       </Grid>
     </>
   );
